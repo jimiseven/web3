ab894c97b0c7e996815a286dc7e365ca05c013b1
Jimi Joe <tano777864@gmail.com>
2024-06-18 12:46:33 -0400

    conexion a db

--- db_ejer/conect.php
+++ db_ejer/conect.php
@@ -2,5 +2,16 @@
     $server = "localhost";
     $user = "root";
     $pass = "";
-    $db = "test";
+    $db = "test1";
 
+    $conexion =  new mysqli($server, $user, $pass, $db);
+
+    if($conexion -> connect_error) {
+        die("conexion fallida". $conexion -> connect_errno);
+
+    }else{
+        echo"conectado";
+    }
+
+
+    ?>
